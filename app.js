429e262157cc24196ccdc2f3e80f3f6dfc9e79ae
festus14 <festusomole14@gmail.com>
2020-12-11 22:14:42 +0100

    still debugging

--- app.js
+++ app.js
@@ -49,5 +49,5 @@ app.use("/api/tool", toolRouter);
 PORT = 
 
 app.listen(3000, () => {
-  console.log(`The server has been created on port ${process.env.PORT || 3000}`);
+  console.log(`The server has been created on port ${process.env.PORT || 3000}, process.env.NODE_ENV... ${process.env.NODE_ENV}`);
 });
