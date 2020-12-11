ecf498c0c5a76439aa8c98545c459d669186a390
festus14 <festusomole14@gmail.com>
2020-12-11 23:12:17 +0100

    added host name

--- app.js
+++ app.js
@@ -47,6 +47,6 @@ app.use("/api/farmer", farmerRouter);
 app.use("/api/tool", toolRouter);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen((PORT, '0.0.0.0'), () => {
     console.log(`Our app is running on port ${ PORT }`);
 });
